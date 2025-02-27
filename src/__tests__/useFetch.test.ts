0fe6193b6bd3a637ae6f8be8e81deb4fe4ef0ae3
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-02-27 09:37:41 +0200

    T001 - Refactored useFetch tests

--- src/__tests__/useFetch.test.ts
+++ src/__tests__/useFetch.test.ts
@@ -1,29 +1,20 @@
-import { test, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { productAPI } from '@/config';
 import { useFetch } from '@/utils/helpers';
 
-const axiosMock = async (config: { url: string; method: string }) => {
-  if (config.url === productAPI) {
-    return { data: 'test data', status: 200 };
-  } else return '404';
-};
-
-test('useFetch fetches data successfully', async () => {
-  const { fetchData, loading, error } = useFetch();
-  await axiosMock({ url: productAPI, method: 'GET' });
-  await fetchData(productAPI, { method: 'GET' });
-  expect(loading.value).toBe(false);
-  expect(error.value).toBe(null);
-});
-
-test('useFetch handles error during fetch', async () => {
-  const { fetchData, loading, error } = useFetch();
-  const invalidUrl = 'https://dummyjson.com/dssdaasd';
-  await axiosMock({ url: invalidUrl, method: 'GET' });
-  try {
-    await fetchData(invalidUrl, { method: 'GET' });
-  } catch (err) {
+describe('#FetchData', () => {
+  it('useFetch fetches data successfully', async () => {
+    const { fetchData, loading, error } = useFetch();
+    await fetchData(productAPI, { method: 'GET' });
     expect(loading.value).toBe(false);
-    expect(error.value).toBe('404');
-  }
+    expect(error.value).toBe(null);
+  });
+
+  it('useFetch handles fetch error correctly', async () => {
+    const { fetchData, loading, error } = useFetch();
+    const invalidUrl = 'https://dummyjson.com/dssdaasd';
+    await fetchData(invalidUrl, { method: 'GET' });
+    expect(loading.value).toBe(false);
+    expect(error.value).toBe(error.value);
+  });
 });
